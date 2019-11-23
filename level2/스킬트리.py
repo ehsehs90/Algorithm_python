b29e1d380fd15987b908f42cb8269916408ae4b7
ehsehs <ehgus1551@naver.com>
2019-11-23 14:19:57 +0900

    스킬트리 코드참조

--- level2/스킬트리.py
+++ level2/스킬트리.py
@@ -1,29 +1,24 @@
-def solution(skill, skill_trees):
+def solution (skill, skill_trees):
     answer = 0
-    count=0
-    list=[]
-    for i in skill:
-        list.append(i)
-    print(list)
-    x=len(skill)
-    problem=False
-    for skill_tree in skill_trees:
-        for i in skill_tree:
-            if i in list[1:x+1]:
-                # print(list)
-                problem=True
+    for str in skill_trees:
+        temp = ""
+        isOk = True
+        for s in str:
+            if skill.find(s) != -1:
+                temp += s
+                print(temp)
+
+        for i in range(len(temp)):
+            if skill[i] != temp[i]:
+                isOk= False
+                print('가자가자')
                 break
-            else:
-                if i == skill[count]:
-                    list.remove(list[0])
-                    count+=1
-                    problem=False
-            count =0
-        if problem==False:
-            answer +=1
-        
-        print(problem)
+
+        if isOk == True:
+            answer += 1 
+
     return answer
+
 
 skill = "CBD"
 skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
