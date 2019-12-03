06900744395834291060c582aff9a46ed6d092e6
ehsehs <ehgus1551@naver.com>
2019-12-03 23:23:08 +0900

    탐욕법

--- level2/구명보트.py
+++ level2/구명보트.py
@@ -1,8 +1,23 @@
 def solution(people, limit):
     answer = 0
+    people.sort()
+    light=0
+    length = len(people)
+    print(length)
+    heavy = length-1
+    print(people)
 
+    while(light<heavy):
 
-    return answer
+        if(people[light]+people[heavy] <= limit):
+            print(people[light])
+            heavy-=1
+            light+=1
+            answer+=1
+        else:
+            heavy-=1
+
+    return length-answer
 
     # 탐욕법
     
@@ -10,3 +25,4 @@ def solution(people, limit):
 people = [70, 50, 80, 50]
 limit = 100
 print(solution(people,limit))
+
