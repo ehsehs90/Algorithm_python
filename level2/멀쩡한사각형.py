3a17faaa29f796f7463d50cf48a64962de45d553
ehsehs <ehgus1551@naver.com>
2019-12-08 19:05:02 +0900

    윈터코딩 | 멀쩡한사각형

--- level2/멀쩡한사각형.py
+++ level2/멀쩡한사각형.py
@@ -1,10 +1,19 @@
+import math
 def solution(w,h):
-    answer = 1
 
-    
+    perfect_rectangular = 0
+    perfect_rectangular = w*h
+
+
+    gcd=math.gcd(w,h)
+    answer = perfect_rectangular - ( w + h - gcd) 
+
+
     return answer
 
 
 w=8
 h=12
+gcd=math.gcd(w,h)
+print(gcd)
 print(solution(w,h))
